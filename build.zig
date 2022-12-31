7010a426f732fdc7280976bcf3c972bfdd56d72c
rohlem <rohlemF@gmail.com>
2022-12-31 15:14:31 +0100

    fix function name, zig fmt, add workaround code

--- build.zig
+++ build.zig
@@ -1,15 +1,20 @@
 const separate_implementation = true; //switch to false to make it work
+const workaround_using_stub_c_file = false; //alternatively enable the workaround
 
-pub fn build1(b: *@import("std").build.Builder) !void {
-	const exe = b.addExecutable("library-example", null);
-	if(separate_implementation) {
-		exe.addCSourceFile("main.c", &.{});
-		exe.addCSourceFile("library.h", &.{"-DLIBRARY_PROVIDE_IMPLEMENTATION"}); //triggers the misleading error message
-	}else{
-		exe.addCSourceFile("main.c", &.{"-DLIBRARY_PROVIDE_IMPLEMENTATION"});
-	}
-	exe.linkLibC();
-	 const run_cmd = exe.run();
+pub fn build(b: *@import("std").build.Builder) !void {
+    const exe = b.addExecutable("library-example", null);
+    if (separate_implementation) {
+        exe.addCSourceFile("main.c", &.{});
+        if (workaround_using_stub_c_file) {
+            exe.addCSourceFile("include_stub.c", &.{ "-DLIBRARY_PROVIDE_IMPLEMENTATION", "-DINCLUDE_FILE_PATH=\"library.h\"" }); //works
+        } else {
+            exe.addCSourceFile("library.h", &.{"-DLIBRARY_PROVIDE_IMPLEMENTATION"}); //triggers the misleading error message
+        }
+    } else {
+        exe.addCSourceFile("main.c", &.{"-DLIBRARY_PROVIDE_IMPLEMENTATION"});
+    }
+    exe.linkLibC();
+    const run_cmd = exe.run();
     run_cmd.step.dependOn(b.getInstallStep());
     b.step("run", "Run the program").dependOn(&run_cmd.step);
 }
---
+++ include_stub.c
@@ -0,0 +1,1 @@
+#include INCLUDE_FILE_PATH
